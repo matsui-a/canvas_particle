eb8660df52a05b5d11273e9a7f3d56b2c53a4b6b
matsui <matsui52@gmail.com>
2015-04-30 07:00:14 +0900

    パーティクルたくさん動かす

--- js/script.js
+++ js/script.js
@@ -5,13 +5,45 @@ window.requestAnimationFrame =
   window.msRequestAnimationFrame ||
   function(cb) {setTimeout(cb, 17);};
 
-var x1 = 5;
-var y1 = 5;
-var x2 = 100;
-var y2 = 5;
-
 var canvas = document.getElementById( "canvas");
 var ctx = canvas.getContext( "2d" );
+var NUM = 20;
+var particles = [];
+
+canvas.width = canvas.height = 500;
+
+for(var i = 0; i < NUM; i++) {
+	positionX = Math.random() * 120;
+	positionY = Math.random() * 20;
+	particle = new Particle(ctx, positionX, positionY);
+	particles.push( particle );
+}
+
+function Particle(ctx, x, y) {
+	this.ctx = ctx;
+	this.x = x || 0;
+	this.y = y || 0;
+}
+
+Particle.prototype.render = function() {
+  this.updatePosition();
+  this.draw();
+}
+
+Particle.prototype.draw = function() {
+  //描画
+  ctx = this.ctx;
+  ctx.beginPath();
+  ctx.fillStyle = "#99ff66";
+  ctx.rect( this.x, this.y, 4, 4 ); // 位置指定
+  ctx.fill();
+  ctx.closePath();
+}
+
+Particle.prototype.updatePosition = function() {
+	this.x += 5;
+	this.y += 5;
+}
 
 //図形を描画する
 render();
@@ -20,26 +52,8 @@ function render() {
   //一度図形を消去
   ctx.clearRect( 0, 0, 500, 500 );
 
-  updateposition();
-  draw( x1, y1 );
-  draw( x2, y2 );
+  particles.forEach(function(e) { e.render(); });
 
   //一定時間おく
   requestAnimationFrame( render );
-}
-
-function updateposition() {
-  x1 += 5;
-  y1 += 5;
-  x2 += 5;
-  y2 += 5;
-}
-
-function draw(posx, posy) {
-  //描画
-  ctx.beginPath();
-  ctx.fillStyle = "#99ff66";
-  ctx.rect( posx, posy, 10, 20 ); // 位置指定
-  ctx.fill();
-  ctx.closePath();
 }
